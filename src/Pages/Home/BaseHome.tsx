33be329679de81ddcdb47ec5f9c7e3780399bb7b
Haxiz <ld.ramirezch14@gmail.com>
2022-09-12 14:43:28 -0600

    add(home): base home page

--- src/Pages/Home/BaseHome.tsx
+++ src/Pages/Home/BaseHome.tsx
@@ -1,5 +1,32 @@
+import {AppShell, useMantineTheme} from "@mantine/core";
+import WFHeader from "../../Layouts/Header/WFHeader";
+import WFFooter from "../../Layouts/Footer/WFFooter";
+import WFNavbar from "../../Layouts/NavBar/WFNavbar";
+import {useState} from "react";
+
 export default function BaseHome() {
+    const theme = useMantineTheme();
+    const [opened, setOpened] = useState(false);
+
     return (
-        <p>holi</p>
+        <AppShell
+            styles={{
+                main: {
+                    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[2],
+                },
+            }}
+            navbarOffsetBreakpoint="sm"
+            navbar={
+                <WFNavbar opened={opened}/>
+            }
+            header={
+                <WFHeader opened={opened} setOpened={setOpened}/>
+            }
+            footer={
+                <WFFooter/>
+            }
+        >
+            <div>Home</div>
+        </AppShell>
     );
 }
