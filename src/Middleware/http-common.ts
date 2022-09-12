c6aa6fdcdaf330e99dea2402bf405cab6d52ebbc
Haxiz <ld.ramirezch14@gmail.com>
2022-09-12 09:37:41 -0600

    update(axios): base api link and headers

--- src/Middleware/http-common.ts
+++ src/Middleware/http-common.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://127.0.0.1:80/v1.0/api",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     }
 });
