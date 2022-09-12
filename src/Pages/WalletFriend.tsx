98d8ed136f7c795b797ec34e7857da13e30a7da6
Haxiz <ld.ramirezch14@gmail.com>
2022-09-12 09:36:26 -0600

    add(cookie): save selected theme to cookie

--- src/Pages/WalletFriend.tsx
+++ src/Pages/WalletFriend.tsx
@@ -3,29 +3,43 @@ import BaseAuth from "./Auth/BaseAuth";
 import BaseHome from "./Home/BaseHome";
 import {ColorScheme, ColorSchemeProvider, MantineProvider} from "@mantine/core";
 import {useState} from "react";
+import {NotificationsProvider} from "@mantine/notifications";
+import {useCookies} from "react-cookie";
 
 export default function WalletFriend() {
-    const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+    const [cookies, setCookie] = useCookies(['theme']);
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(cookies.theme);
 
     const toggleColorScheme = (value?: ColorScheme) =>
-        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+        setColorScheme(value || (colorScheme === 'dark' ?
+                (() => {
+                    setCookie('theme', 'light', {path: '/', sameSite: 'lax'});
+                    return 'light';
+                })
+                : (() => {
+                    setCookie('theme', 'dark', {path: '/', sameSite: 'lax'});
+                    return 'dark';
+                })
+        ));
 
     return (
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
             <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme}}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/auth">
-                            <BaseAuth/>
-                        </Route>
-                        <Route path="/home">
-                            <BaseHome/>
-                        </Route>
-                        <Route exact path="/">
-                            <Redirect to="/auth"/> {/* TODO: Validate if user is logged in or not */}
-                        </Route>
-                    </Switch>
-                </BrowserRouter>
+                <NotificationsProvider>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route path="/auth">
+                                <BaseAuth/>
+                            </Route>
+                            <Route path="/home">
+                                <BaseHome/>
+                            </Route>
+                            <Route exact path="/">
+                                <Redirect to="/auth"/> {/* TODO: Validate if user is logged in or not */}
+                            </Route>
+                        </Switch>
+                    </BrowserRouter>
+                </NotificationsProvider>
             </MantineProvider>
         </ColorSchemeProvider>
     );
