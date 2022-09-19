a0380512a0702e502943de6326537962987d57df
Haxiz <ld.ramirezch14@gmail.com>
2022-09-19 16:17:16 -0600

    add(logo): logo redirect on header

--- src/Layouts/Header/WFHeader.tsx
+++ src/Layouts/Header/WFHeader.tsx
@@ -3,10 +3,24 @@ import BlackLogo from "../../Assets/Images/Logos/Wallet-Friend-Simple-logos_blac
 import WhiteLogo from "../../Assets/Images/Logos/Wallet-Friend-Simple-logos_white.png";
 import ThemeButton from "../../Components/ThemeButton/ThemeButton";
 import LogoutButton from "../../Components/LogoutButton/LogoutButton";
-import {useMatch} from "react-router-dom";
+import {useMatch, useNavigate} from "react-router-dom";
 
 export default function WFHeader({opened, setOpened}: { opened: boolean, setOpened: (opened: (o: any) => boolean) => void }) {
-    let match = useMatch("/auth/*");
+    let matchAuth = useMatch("/auth/*");
+    let matchHome = useMatch("/home/*");
+    const navigate = useNavigate();
+
+    const onClickLogo = () => {
+        if (matchAuth) {
+            navigate("/auth");
+        }
+        else if (matchHome) {
+            navigate("/home");
+        }
+        else {
+            navigate("/");
+        }
+    }
 
     const HEADER_HEIGHT = 60;
 
@@ -28,9 +42,9 @@ export default function WFHeader({opened, setOpened}: { opened: boolean, setOpen
             <Container className={classes.inner} fluid>
                 <Group>
                     <MediaQuery largerThan="sm" styles={{display: 'none'}}>
-                        <Burger opened={opened} onClick={() => { setOpened((o) => !o) }} size="sm" hidden={match !== null}/>
+                        <Burger opened={opened} onClick={() => { setOpened((o) => !o) }} size="sm" hidden={matchAuth !== null}/>
                     </MediaQuery>
-                    <Image src={theme.colorScheme === 'dark' ? WhiteLogo : BlackLogo} width={200} withPlaceholder/>
+                    <Image src={theme.colorScheme === 'dark' ? WhiteLogo : BlackLogo} width={200} withPlaceholder onClick={() => {onClickLogo()}} sx={{ cursor: "pointer"}}/>
                 </Group>
                 <Group>
                     <ThemeButton/>
