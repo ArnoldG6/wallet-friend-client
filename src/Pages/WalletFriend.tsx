88bea617c5bb6c7ce7cc13ce54285401a429e7be
Haxiz <ld.ramirezch14@gmail.com>
2022-09-19 13:43:50 -0600

    fix(404): add error element in case of not found or fail load

--- src/Pages/WalletFriend.tsx
+++ src/Pages/WalletFriend.tsx
@@ -5,17 +5,19 @@ import WFShell from "../Layouts/Shell/WFShell";
 import Login from "./Auth/Login/Login";
 import SignUp from "./Auth/SignUp/SignUp";
 import RequireAuth from "../Routes/RequireAuth";
+import NotFound from "../Components/NotFound/NotFound";
 
 export default function WalletFriend() {
     let router = createBrowserRouter(
         createRoutesFromElements(
-            <Route path="/" element={<WFShell/>}>
-                <Route path="auth" element={<BaseAuth/>}>
-                    <Route path="login" element={<Login/>}/>
-                    <Route path="register" element={<SignUp/>}/>
+            <Route path="/" element={<WFShell/>} errorElement={<NotFound/>}>
+                <Route path="auth" element={<BaseAuth/>} errorElement={<NotFound/>}>
+                    <Route path="login" element={<Login/>} errorElement={<NotFound/>}/>
+                    <Route path="register" element={<SignUp/>} errorElement={<NotFound/>}/>
+                    <Route path="*" element={<NotFound/>}/>
                 </Route>
-                <Route path="home" element={<RequireAuth> <BaseHome/> </RequireAuth>}>
-                    <Route path="meme" element={<RequireAuth> <div>memes</div> </RequireAuth>}/>
+                <Route path="home" element={<RequireAuth> <BaseHome/> </RequireAuth>} errorElement={<NotFound/>}>
+                    <Route path="meme" element={<RequireAuth> <div>memes</div> </RequireAuth>} errorElement={<NotFound/>}/>
                 </Route>
             </Route>
         )
